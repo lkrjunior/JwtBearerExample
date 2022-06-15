482a660fcdea88b7f3f0d99d76edd487878763ed
lkrjunior <lkrjunior@terra.com.br>
2022-06-15 13:37:24 -0300

    Second project

---
+++ JwtBearerExample.API.Client/Program.cs
@@ -0,0 +1,41 @@
+﻿#region TopLevelStatements
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder);
+
+var app = builder.Build();
+
+Configure(app);
+
+app.Run();
+
+#endregion
+
+#region Startup
+
+static void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+}
+
+static void Configure(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+}
+
+#endregion
