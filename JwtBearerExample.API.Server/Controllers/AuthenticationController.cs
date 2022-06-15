acfec483ca76de0f7b3a20114766504665dbf4f6
lkrjunior <lkrjunior@terra.com.br>
2022-06-15 14:23:03 -0300

    Server

---
+++ JwtBearerExample.API.Server/Controllers/AuthenticationController.cs
@@ -0,0 +1,72 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtBearerExample.API.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthenticationController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet(Name = "GetToken")]
+    public IActionResult GetToken()
+    {
+        if (!double.TryParse(_configuration["ExpiresTokenInMinutes"], out var expiresTokenInMinutes))
+        {
+            expiresTokenInMinutes = 10;
+        }
+
+        var expires = DateTime.UtcNow.AddMinutes(expiresTokenInMinutes);
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(_configuration["SecretKey"]);
+
+        var subject = new ClaimsIdentity(new Claim[]
+        {
+                new Claim(ClaimTypes.Name, "Admin"),
+                new Claim(ClaimTypes.Role, "Admin")
+        });
+
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = _configuration["Issuer"],
+            Audience = _configuration["Audience"],
+            Subject = subject,
+            Expires = expires,
+            SigningCredentials = signingCredentials
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        var accessToken = tokenHandler.WriteToken(token);
+
+        var result = new 
+        {
+            AccessToken = accessToken,
+            ExpiresAtUtc = expires
+        };
+
+        return Ok(result);
+    }
+
+    [HttpGet("NeedToken")]
+    [Authorize]
+    public IActionResult NeedToken()
+    {
+        return Ok();
+    }
+}
+
--- JwtBearerExample.API.Server/Program.cs
+++ JwtBearerExample.API.Server/Program.cs
@@ -1,8 +1,13 @@
 ﻿#region TopLevelStatements
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureServices(builder.Services);
+ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
@@ -14,13 +19,67 @@ app.Run();
 
 #region Startup
 
-static void ConfigureServices(IServiceCollection services)
+static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
 {
     services.AddControllers();
 
     services.AddEndpointsApiExplorer();
 
-    services.AddSwaggerGen();
+    services.AddSwaggerGen(x =>
+    {
+        x.SwaggerDoc("v1", new OpenApiInfo { Title = "Challenge.API", Version = "v1" });
+
+        x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization Header",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+        });
+
+        x.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+    });
+
+    var key = Encoding.ASCII.GetBytes(configuration["SecretKey"]);
+    var validIssuer = configuration["Issuer"];
+    var validAudience = configuration["Audience"];
+
+    services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero,
+            ValidIssuer = validIssuer,
+            ValidAudience = validAudience
+        };
+    });
 }
 
 static void Configure(WebApplication app, IWebHostEnvironment environment)
@@ -33,8 +92,9 @@ static void Configure(WebApplication app, IWebHostEnvironment environment)
 
     app.UseHttpsRedirection();
 
+    app.UseAuthentication();
     app.UseAuthorization();
-
+    
     app.MapControllers();
 }
 
